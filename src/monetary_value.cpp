27614a6e418c4c00c4b09ceee90cd14bd99f75ad
Shawn Evans <shawnevans328@gmail.com>
2025-12-01 23:23:24 -0500

    update

--- src/monetary_value.cpp
+++ src/monetary_value.cpp
@@ -6,11 +6,12 @@ class MonetaryValue {
 
     public:
         MonetaryValue(int cents) { this->cents = cents; };
+        MonetaryValue(double amount) {this->cents = (int) (amount * 100);}
         bool is_negative() { return cents < 0; };
 };
 
 int main() {
-    MonetaryValue val(50);
+    MonetaryValue val(50.22);
 
     std::cout << val.cents << "\n";
     std::cout << val.is_negative() << "\n";
