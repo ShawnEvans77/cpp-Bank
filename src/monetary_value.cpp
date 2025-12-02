05c6ede9923c613e2050970157da15240f96b0e4
Shawn Evans <shawnevans328@gmail.com>
2025-12-01 23:49:30 -0500

    update

--- src/monetary_value.cpp
+++ src/monetary_value.cpp
@@ -5,17 +5,16 @@ class MonetaryValue {
         int cents;
 
     public:
-        MonetaryValue(int cents) { this->cents = cents; };
+        MonetaryValue(int cents) { this->cents = cents; }
         MonetaryValue(double amount) {this->cents = (int) (amount * 100);}
         bool is_negative() { return cents < 0; };
-        
+        int get_cents() { return cents; }
 };
 
 int main() {
     MonetaryValue val(50.22);
 
-    std::cout << val.cents << "\n";
-    std::cout << val.is_negative() << "\n";
+    std::cout << val.get_cents() << "\n";
 
     return 0;
 }
---
+++ src/name.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+class Name {
+    private:
+        std::string first;
+        std::string last;
+
+    public:
+        Name(std::string first, std:: string last) {
+            this->first = first;
+            this->last = last;
+        }
+
+        void set_first(std::string first) { this->first = first;}
+        void set_last(std::string first) { this->first = first;}
+        std::string get_first() {return first;}
+        std::string get_last() {return last;}
+        
+};
