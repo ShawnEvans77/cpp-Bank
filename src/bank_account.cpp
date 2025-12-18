5198d6dc0c1c43069ac3ecc88360b36c42a7d2fa
Shawn Evans <shawnevans328@gmail.com>
2025-12-17 21:39:57 -0500

    msg

---
+++ src/bank_account.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <monetary_value.h>
+
+class BankAccount {
+    private:
+        std::string name;
+        MonetaryValue x;
+}
--- src/monetary_value.cpp
+++ src/monetary_value.cpp
@@ -5,14 +5,24 @@ class MonetaryValue {
         int cents;
 
     public:
-        MonetaryValue(int cents) { this->cents = cents; }
-        MonetaryValue(double amount) {this->cents = (int) (amount * 100);}
-        bool is_negative() { return cents < 0; };
-        int get_cents() { return cents; }
+        MonetaryValue(int cents) : cents(cents) // we call this the member initializer list. 
+            {}
+
+        MonetaryValue(double amount) {
+            this->cents = (int) (amount * 100);
+        }
+
+        bool is_negative() { 
+            return cents < 0;
+         };
+
+        int get_cents() { 
+            return cents; 
+        }
 };
 
 int main() {
-    MonetaryValue val(50.22);
+    MonetaryValue val(455);
 
     std::cout << val.get_cents() << "\n";
 
